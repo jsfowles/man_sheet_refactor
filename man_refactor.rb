8cc425bdfce44abbd10ea538288433130536dde9
Jacob Fowles <js.fowles@gmail.com>
2016-05-20 22:35:12 -0600

    unable to redirect form data to differct view

--- man_refactor.rb
+++ man_refactor.rb
@@ -1,6 +1,18 @@
 require 'sinatra'
 require 'pry'
+require 'shotgun'
 
-get '/' do
-  'hello world'
+get '/main_menu' do
+  params[:message]
+  erb :main_menu
+end
+
+get '/mans' do
+  params[:message]
+erb :mans
+end
+
+post '/main_menu' do
+ params[:message]
+ erb :main_menu
 end
