45ca3f850ff1a2d4633ecb64fe7c4ff36d4b7ca0
Jacob Fowles <js.fowles@gmail.com>
2016-05-21 22:44:12 -0600

    all functions working, ready for production

--- Gemfile
+++ Gemfile
@@ -3,4 +3,3 @@ ruby '2.3.0'
 
 gem 'pry', '~> 0.10.3'
 gem 'sinatra', '~> 1.4.7'
-gem 'shotgun', '~> 0.9.1'
--- man_refactor.rb
+++ man_refactor.rb
@@ -1,18 +1,50 @@
 require 'sinatra'
 require 'pry'
-require 'shotgun'
 
 get '/main_menu' do
-  params[:message]
   erb :main_menu
 end
 
 get '/mans' do
-  params[:message]
-erb :mans
+  erb :mans
+end
+
+get '/cp' do
+  erb :cp
+end
+
+get '/mv' do
+  erb :mv
+end
+
+get '/cd' do
+  erb :cd
 end
 
 post '/main_menu' do
- params[:message]
- erb :main_menu
+  message = params[:message]
+  if message.to_i == 1
+    redirect '/mans'
+  elsif message.to_i == 2
+    redirect 'https://www.google.com'
+  else
+    redirect '/main_menu'
+  end
+  erb :main_menu
+end
+
+post '/mans' do
+  @mans = params[:selection]
+  if @mans.to_i == 1
+    redirect '/cp'
+  elsif @mans.to_i == 2
+    redirect '/mv'
+  elsif @mans.to_i == 3
+    redirect '/cd'
+  elsif @mans.to_i == 4
+    redirect '/main_menu'
+  else
+    redirect '/mans'
+  end
+  erb :mans
 end
